d131602e54946b0e747b22414d96162b66f2ddaf
Gino Boca <gnoboca@gmail.com>
2022-07-29 12:45:15 -0300

    modificacion pagina contacto

--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -10,9 +10,10 @@ export default function Proyectos() {
        <div className='contacto-links'>
           <a href='https://ginoboca1.github.io/Natural-Food/'>Natural Food</a>
           <a href='https://ginoboca1.github.io/TaskApp/'>Task App</a>
-          <a href='https://ginoboca1.github.io/CalculadoraReactJS/'>Calculadora ReactJS</a>
-          <a href='https://ginoboca1.github.io/My_daily_Tech/'>Blog review</a>
+          <a href='https://ginoboca1.github.io/CalculadoraReactJS/'>Calculadora React</a>
+          <a href='https://ginoboca1.github.io/relojDigital.js/'>Reloj Digital</a>
           <a href='https://ginoboca1.github.io/UserAuth/'>User Authenticator </a>
+          <a href='https://ginoboca1.github.io/Tech-commerce/'>E-commerce-(en desarrollo)</a>
           <a href='https://github.com/Ginoboca1?tab=repositories'> repositorio github</a>
        </div>
     </div>
