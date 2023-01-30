f82274480242672c13119ab670d9de3f5ca4cfe5
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-01-30 09:28:54 -0300

    links updated

--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -12,7 +12,7 @@ export default function Proyectos() {
           <a href='https://ginoboca1.github.io/TaskApp/'>Task App</a>
           <a href='https://ginoboca1.github.io/CalculadoraReactJS/'>Calculadora React</a>
           <a href='https://ginoboca1.github.io/relojDigital.js/'>Reloj Digital</a>
-          <a href='https://ginoboca1.github.io/UserAuth/'>User Authenticator </a>
+          <a href='https://ginoboca1.github.io/userAuthFirebase/'>Firebase Auth </a>
           <a href='https://ginoboca1.github.io/Tech-commerce/'>E-commerce-(en desarrollo)</a>
           <a href='https://github.com/Ginoboca1?tab=repositories'> repositorio github</a>
        </div>
