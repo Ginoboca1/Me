01487182e0fb8997614616ec02b24c2645c9547f
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2024-01-31 08:54:01 -0300

    links fixed

--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -15,14 +15,16 @@ export default function Proyectos() {
       </div>
       <h1>My Projects</h1>
       <div className="projects-links">
-        <a href="ginoboca1.github.io/React-Restaurant/">React restaurant</a>
+        <a href="https://ginoboca1.github.io/React-Restaurant/">
+          React restaurant
+        </a>
         <a href="https://github.com/Ginoboca1/CalculadoraReactJS">
           React Calculator
         </a>
         <a href="https://ginoboca1.github.io/userAuthFirebase/">
           Firebase Auth{" "}
         </a>
-        <a href="https://ws-chat-client-theta.vercel.app/">Socket.io Chat</a>
+        <a href="https://github.com/Ginoboca1/ws-chat">Socket.io Chat</a>
         <a href="https://fallacious-whitefish-1fb.notion.site/Nest-API-Documentation-0c3eb12c7e0f4207a548b2b9fc8ed102">
           NestJS API
         </a>
