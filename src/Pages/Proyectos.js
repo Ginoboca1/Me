f70b1ee4d707b3a9d047e9a24d443e972eeef5a9
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2024-02-15 16:34:17 -0300

    updated links

--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -27,7 +27,7 @@ export default function Proyectos() {
         <a href="https://fallacious-whitefish-1fb.notion.site/WS-Chat-1-c1205ecdb38c417fa5f5086f834d15a0">
           Socket.io Chat
         </a>
-        <a href="https://fallacious-whitefish-1fb.notion.site/Nest-API-Documentation-0c3eb12c7e0f4207a548b2b9fc8ed102">
+        <a href="https://ws-chat-docs.notion.site/Nest-js-API-4037fd68c24e474ab6553a6e3137dd78?pvs=74">
           NestJS API
         </a>
         <a href="https://megarocketgym-app.vercel.app/auth/home">
